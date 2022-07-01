f44530a902b748e49fc8c2909d4b070efe884712
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-07-01 16:17:09 +0800

    Menambahkan folder pertemuan6

---
+++ pertemuan6/app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="mt-4">
+        <div class="h-100 p-5 text-white bg-dark rounded-3">
+            <h2>Selamat Datang di Website Saya!</h2>
+            <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
+            <button class="btn btn-outline-light" type="button">Example button</button>
+        </div>
+    </div>
+</div>
