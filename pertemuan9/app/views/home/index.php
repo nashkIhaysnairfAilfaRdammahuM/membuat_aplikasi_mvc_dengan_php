b2f740709beb7eaf5328a03a8f69faeb17fabc35
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-07-04 21:26:49 +0800

    Menambahkan folder pertemuan9

---
+++ pertemuan9/app/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="mt-4">
+        <div class="h-100 p-5 text-white bg-dark rounded-3">
+            <h2>Selamat Datang di Website Saya!</h2>
+            <p>Halo, nama saya <?= $data['nama']; ?></p>
+            <button class="btn btn-outline-light" type="button">Example button</button>
+        </div>
+    </div>
+</div>
