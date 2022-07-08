2fd4b65d53049337e8183771cdc7ecea20339400
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-07-08 23:49:09 +0800

    Menambahkan folder pertemuan12

---
+++ pertemuan12/app/models/Mahasiswa_model.php
@@ -0,0 +1,78 @@
+<?php
+class Mahasiswa_model {
+    // private $mhs = [
+    //     [
+    //         "nama" => "Sandhika Galih",
+    //         "nrp" => "043040023",
+    //         "email" => "sandhikagalih@unpas.ac.id",
+    //         "jurusan" => "Teknik Informatika"
+    //     ],
+    //     [
+    //         "nama" => "Doddy Ferdiansyah",
+    //         "nrp" => "133050321",
+    //         "email" => "doddy@gmail.com",
+    //         "jurusan" => "Teknik Mesin"
+    //     ],
+    //     [
+    //         "nama" => "Erik Doank",
+    //         "nrp" => "163030123",
+    //         "email" => "erik@yahoo.com",
+    //         "jurusan" => "Teknik Industri"
+    //     ],
+    // ];
+
+    private $table = 'mahasiswa';
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
+    public function getAllMahasiswa() {
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id) {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
+    }
+
+    public function tambahDataMahasiswa($data) {
+        $query = "INSERT INTO mahasiswa VALUES (NULL, :nama, :nrp, :email, :jurusan)";
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+        
+        return $this->db->rowCount();
+    }
+
+    public function hapusDataMahasiswa($id) {
+        $query = "DELETE FROM mahasiswa WHERE id = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function ubahDataMahasiswa($data) {
+        $query = "UPDATE mahasiswa SET nama = :nama, nrp = :nrp, email = :email, jurusan = :jurusan WHERE id = :id";
+        $this->db->query($query);
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nrp', $data['nrp']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+        $this->db->bind('id', $data['id']);
+
+        $this->db->execute();
+        
+        return $this->db->rowCount();
+    }
+}
---
+++ pertemuan12/app/views/mahasiswa/index.php
@@ -0,0 +1,68 @@
+<div class="container mt-3">
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?php Flasher::flash(); ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <button type="button" class="btn btn-primary tombolTambahData" data-bs-toggle="modal" data-bs-target="#formModal">Tambah Data Mahasiswa</button>
+            <h3 class="mt-3">Daftar Mahasiswa</h3>
+            <ul class="list-group">
+                <?php foreach($data['mhs'] as $mhs) : ?>
+                <li class="list-group-item">
+                    <?= $mhs['nama'] ?>
+                    <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'] ?>" class="badge text-bg-danger float-end ms-1" onclick="return confirm('yakin?')">hapus</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id'] ?>" class="badge text-bg-warning float-end ms-1 tampilModalUbah" data-bs-toggle="modal" data-bs-target="#formModal" data-id="<?= $mhs['id'];  ?>">ubah</a>
+                    <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="badge text-bg-primary float-end ms-1">detail</a>
+                </li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <input type="hidden" name="id" id="id">
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" name="nama">
+                    </div>
+                    <div class="mb-3">
+                        <label for="nrp" class="form-label">NRP</label>
+                        <input type="number" class="form-control" id="nrp" name="nrp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="jurusan" class="form-label">Jurusan</label>
+                        <select class="form-select" id="jurusan" name="jurusan" aria-label="Jurusan">
+                            <option value="Teknik Informatika">Teknik Informatika</option>
+                            <option value="Teknik Industri">Teknik Industri</option>
+                            <option value="Teknik Pangan">Teknik Pangan</option>
+                            <option value="Teknik Planologi">Teknik Planologi</option>
+                            <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+                        </select>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Tambah Data</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
