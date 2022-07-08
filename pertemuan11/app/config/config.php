0ce0ab26e986aba1de35bdbddfd217f30c9a7450
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-07-08 20:08:02 +0800

    Menambahkan folder pertemuan11

---
+++ pertemuan11/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('BASEURL', 'http://localhost/web_programming_unpas/membuat_aplikasi_mvc_dengan_php/pertemuan11/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
