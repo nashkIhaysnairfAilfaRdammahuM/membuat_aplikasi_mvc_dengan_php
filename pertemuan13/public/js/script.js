68ff486ef67a0b2bfaf2333b070e6612e439f2ba
nashkIhaysnairfAilfaRdammahuM <nashkihaysnairfailfardammahum@gmail.com>
2022-07-09 13:58:51 +0800

    Menambahkan folder pertemuan13

---
+++ pertemuan13/public/js/script.js
@@ -0,0 +1,33 @@
+$(function() {
+    $('.tombolTambahData').on('click', function() {
+        $('#judulModal').html('Tambah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Tambah Data');
+        $('#nama').val('');
+        $('#nrp').val('');
+        $('#email').val('');
+        $('#jurusan').val('');
+        $('#id').val('');
+    });
+
+    $('.tampilModalUbah').on('click', function() {
+        $('#judulModal').html('Ubah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+        $('.modal-body form').attr('action', 'http://localhost/web_programming_unpas/membuat_aplikasi_mvc_dengan_php/pertemuan13/public/mahasiswa/ubah');
+
+        const id = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost/web_programming_unpas/membuat_aplikasi_mvc_dengan_php/pertemuan13/public/mahasiswa/getUbah',
+            data: {id : id},
+            method: 'post',
+            dataType: 'json',
+            success: function(data) {
+                $('#nama').val(data.nama);
+                $('#nrp').val(data.nrp);
+                $('#email').val(data.email);
+                $('#jurusan').val(data.jurusan);
+                $('#id').val(data.id);
+            }
+        });
+    });
+});
